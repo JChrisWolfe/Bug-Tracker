a860c34f33fecf3db73de71c76b3523da940a9f2
James C. Wolfe <jcwolfe0729@gmail.com>
2020-04-08 16:24:03 -0400

    Removed class Bug_Types, added class Projects in bug_track_system/models.py

--- bug_tracker/bug_track_system/models.py
+++ bug_tracker/bug_track_system/models.py
@@ -9,7 +9,5 @@ class Bugs(models.Model):
     bug_description = models.TextField(max_length=3000)
     bug_tester_id = models.IntegerField()
 
-class Bug_Types(models.Model):
-    bug_type_id = models.IntegerField(primary_key=True)
-    bug_type_title = models.CharField(max_length=20)
-    bug_type_description = models.TextField(max_length=2000)
+class Projects(models.Model):
+    pass
